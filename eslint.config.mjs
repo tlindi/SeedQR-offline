11885940c6c1ae2ba72c608304058540a852fd2e
tlindi <tomi@lind.fi>
2026-08-16 16:52:50 +0300

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,26 +1,19 @@
-// eslint.config.js
-import js from "@eslint/js";
-
-export default [
-  js.configs.recommended,
-  {
-    languageOptions: {
-      ecmaVersion: 5,
-      sourceType: "script",
-      globals: {
-        browser: true
-      }
-    },
-    rules: {
-      "no-var": "off",
-      "no-const": "error",
-      "no-let": "error",
-      "no-arrow-functions": "error",
-      "no-class": "error",
-      "no-template-literals": "error",
-      "no-rest-parameters": "error",
-      "no-spread": "error",
-      "no-destructuring": "error"
-    }
+// eslint.config.mjs
+export default {
+  languageOptions: {
+    ecmaVersion: 5,
+    sourceType: "script",
+    globals: { browser: true }
+  },
+  rules: {
+    "no-var": "off",
+    "no-const": "error",
+    "no-let": "error",
+    "no-arrow-functions": "error",
+    "no-class": "error",
+    "no-template-literals": "error",
+    "no-rest-parameters": "error",
+    "no-spread": "error",
+    "no-destructuring": "error"
   }
-];
+};
